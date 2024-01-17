85262df2c71abf12f0f182bc32ae0073fe281a4b
Septimiu-Calin Bodica <p0licat@users.noreply.github.com>
2024-01-17 19:11:28 +0100

    Static analysis pass of CytoscapeGraph

--- blog-site/app/twitter/page.tsx
+++ blog-site/app/twitter/page.tsx
@@ -1,7 +1,10 @@
+import { CytoscapeGraph } from "../components/CytoscapeGraph/CytoscapeGraph";
+
 export default function VerifyPage() {
   return (
     <>
       <h1>X page</h1>
+      <CytoscapeGraph></CytoscapeGraph>
       <p>
         This page is intended to verify that Redux state is persisted across
         page navigations.
